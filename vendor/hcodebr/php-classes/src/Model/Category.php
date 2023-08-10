2a8f901f1bb195250127fcb752a9fbd17710f411
Luiza Ribeiro <luiza@oneweb.com.br>
2023-08-10 15:05:34 -0300

    categorias site

--- vendor/hcodebr/php-classes/src/Model/Category.php
+++ vendor/hcodebr/php-classes/src/Model/Category.php
@@ -17,6 +17,7 @@ class Category extends Model {
             ":descategory" => $this->getdescategory()
         ));
         $this->setData($results[0]);
+        Category::updateFile();
     }
 
     public function get($idcategory) {
@@ -32,5 +33,15 @@ class Category extends Model {
         $sql->query("DELETE FROM tb_categories WHERE idcategory = :idcategory", array(
             ":idcategory" => $this->getidcategory()
         ));
+        Category::updateFile();
+    }
+
+    public static function updateFile() {
+        $categories = Category::listAll();
+        $html = [];
+        foreach ($categories as $row) {
+            array_push($html, '<li><a href="/categories/'.$row['idcategory'].'">'.$row['descategory'].'</a></li>');
+        }
+        file_put_contents($_SERVER['DOCUMENT_ROOT'].DIRECTORY_SEPARATOR."views".DIRECTORY_SEPARATOR."categories-menu.html", implode('', $html));
     }
 }
